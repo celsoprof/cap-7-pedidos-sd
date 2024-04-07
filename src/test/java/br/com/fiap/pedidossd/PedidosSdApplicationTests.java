e1d05bdf2c4170def61c5dfcbface88bb0623a7e
Celso Furtado <celso.furtado@sp.senai.br>
2024-04-06 23:44:07 -0300

    sd

---
+++ src/test/java/br/com/fiap/pedidossd/PedidosSdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pedidossd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosSdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
